fc2fd123b47ba3e7b8d943b5a5790c6b40f33bdc
Joseph Ibochi <josephibochi@gmail.com>
2022-06-22 03:30:47 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.write("<h1>My info</h1> <br><br>")
+document.write("<b>Name:</b> Joseph Ibochi <br>")
+document.write("<b>Height:</b> 6'1feet <br>")
+document.write("<b>Country:</b> Nigeria")
